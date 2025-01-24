30932f7aebdcffff024b7e93984dffa8c329658a
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-01-25 02:22:31 +0545

    role assignment to user code implemented

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -2,18 +2,25 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Enum\RoleEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 
 class AuthApiController extends Controller
 {
     public function register(UserRegisterRequest $request){
-        $token = User::create($request->validated())->getAuthToken();
+        $token = null;
+        DB::transaction(function () use($request){
+            $user = User::create($request->validated());
+            $user->assignRole(RoleEnum::USER->value);
+            $token = $user->getAuthToken();
+        });
         return Response::successJson('User Registered', compact('token'));
     }
 
